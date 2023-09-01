f619bb2326a3db8973e3856d61a6d8d62e4b3509
BreElle Wells <breellejordyn@gmail.com>
2023-09-01 11:26:40 -0600

    create main, new game, and load game menus

--- lib/cli.py
+++ lib/cli.py
@@ -1,8 +1,84 @@
 from classes.player import Player
-looping = True
 
-player = Player()
-high_score = 0
+
+# print methods
+def print_login_screen():
+    print("                            ")
+    print("+--------------------------+")
+    print("| WELCOME TO CAVE CRAWLER! |")
+    print("+--------------------------+")
+    print("| Options:                 |")
+    print("| 1. New Game              |")
+    print("| 2. Load Game             |")
+    print("| x: Exit Game             |")
+    print("+--------------------------+")
+    print("                            ")
+
+
+def print_new_game_header():
+    print("                            ")
+    print("+--------------------------+")
+    print("|         NEW GAME         |")
+    print("+--------------------------+")
+    print("                            ")
+
+
+def print_load_game_header():
+    print("                            ")
+    print("+--------------------------+")
+    print("|         LOAD GAME        |")
+    print("+--------------------------+")
+    print("                            ")
+
+
+# menus
+def view_new_game_menu():
+    print_new_game_header()
+    print("Create an account:")
+    # print("\nUsername must be between 3 and 20 characters.")
+    username = input("\nUsername: ")
+
+    # print("\nPassword must be between 3 and 20 characters.")
+    password = input("Password: ")
+
+    # create account class?
+    # account = Account(username, password)
+
+    deciding = True
+
+    while deciding:
+        decision = input("\nCreate account? [y/n]: ")
+        decision.lower()
+
+        if decision == "y":
+            # account.save()
+            print(f"\nWelcome, {username.upper()}!")
+            print("\nBeginning your adventure...")
+            mainGame()
+            deciding = False
+        elif decision == "n":
+            print("\nAccount creation canceled.")
+            print("Returning to login screen...")
+            deciding = False
+        else:
+            print("\nNot a valid input!")
+
+
+def view_load_game_menu():
+    print_load_game_header()
+    print("Enter your account details:")
+
+    # has to match info from database
+    username = input("\nUsername: ")
+    password = input("Password: ")
+
+    # if it does match
+    print(f"\nWelcome back, {username.upper()}!")
+    print(f"\nHigh Score: {high_score}")
+    print("\nBeginning your adventure...")
+    mainGame()
+
+
 def mainGame():
     game_looping = True
     current_level = 0
@@ -10,20 +86,21 @@ def mainGame():
     outcome = "defeat"
 
     while game_looping:
-    
-        print("""
+        print(
+            """
 -----------------------------------------------------------------------------
-        You find yourself in a large, dark cave. 
-        Ahead of you you see a tunnel branching off to the left, and one to the right. 
-        Which do you choose? 
+        You find yourself in a large, dark cave.
+        Ahead of you you see a tunnel branching off to the left, and one to the right.
+        Which do you choose?
         (1) Left
         (2) Right
         (3) Stay here
         (x) Exit to main menu
-                """)
+                """
+        )
         choice = input("Enter your choice: (1, 2, 3, or x): ")
         if choice in ("1", "2"):
-            current_level +=1
+            current_level += 1
             highest_level_reached = max(highest_level_reached, current_level)
             if current_level == 5:
                 print("VICTORY! YOU'VE MADE IT OUT!")
@@ -37,23 +114,27 @@ def mainGame():
             game_looping = False
         else:
             print("Not a valid input!")
-    
+
     return (outcome, highest_level_reached)
 
+
+# main method code
+looping = True
+
+player = Player()
+high_score = 0
+
 while looping:
-    # MAIN MENU
-    print(f"""
-        WELCOME TO CAVE CRAWLER!
-        High Score: {high_score}
-        To begin, press 'x'
-        To exit, enter 'quit'
-          """)
-    choice = input("Begin your adventure? ")
-    if choice == "x":
-        (outcome, highest_level_reached) = mainGame()
-        high_score = max(highest_level_reached, high_score)
-    elif choice == "quit":
+    print_login_screen()
+    choice = input("Input your choice: ")
+    choice = choice.lower()
+
+    if choice == "1":
+        view_new_game_menu()
+    elif choice == "2":
+        view_load_game_menu()
+    elif choice == "x":
         looping = False
+        print("Exiting game...")
     else:
         print("Not a valid input!")
-    
