35ce00f12cfd40225c0f4e3ede2028938905c80b
Hallie Sala <halliesala19@gmail.com>
2023-09-05 13:27:26 -0400

    Tues morning

--- lib/language_buddy.py
+++ lib/language_buddy.py
@@ -1,9 +1,17 @@
 # Import needed classes from models.py
 from colorama import Fore
 import time
+import requests
+import json
+
+API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+API_KEY = "PLACEHOLDER"
 
 
 class LanguageBuddy:
+
+    source_language = "English"
+    target_language = "Russian"
 
     def run(self):
         print("Welcome to Language Buddy!")
@@ -43,7 +51,38 @@ class LanguageBuddy:
             pass
 
     def select_language(self):
-        pass
+        while True:
+            print("Select the language you want to learn")
+            self.user_input = input(">>> ")
+            headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {API_KEY}"
+                }
+            validate_language_data = {
+                "model": "gpt-3.5-turbo",
+                "messages": [{
+                    "role": "user", 
+                    "content": f"Does gpt-3.5-turbo support the following language: {self.user_input}? Return 'TRUE' or 'FALSE'. Do not return any other text."
+                }],
+                "temperature": 0.7
+            }
+            # Make the POST request
+            response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(validate_language_data))
+
+            # Get the JSON response
+            response_data = response.json()
+
+            # Extract the sentence
+            language_validated = True if response_data['choices'][0]['message']['content'] == 'TRUE' else False
+            
+            if language_validated:
+                self.target_language = self.user_input
+                print(f"Your language has been set to {self.target_language}")
+                self.main_menu()
+                break
+            else:
+                print("Invalid input. Please select a supported language")
+
 
     def view_stats(self):
         pass
@@ -81,18 +120,78 @@ class LanguageBuddy:
             pass
 
     def translate(self):
-        # Generate a source-language sentence for user to translate: 
-        # ex., "The cat is on the roof." 
-        # Then, user inputs translation. 
+        # Generate a source-language sentence for user to translate:
+        # ex., "The cat is on the roof."
+        # Then, user inputs translation.
         # Via OpenAI API, we evaluate translation and return comments.
-        # Comments may include praise ("Great job!"), corrections 
+        # Comments may include praise ("Great job!"), corrections
         # ("Not quite -- a better translation is ..."), encouragement ("You've translated 10 sentences today!").
         # As long as user doesn't press i.e., 'Quit', we keep going
         # Store sentence and translation in db, or store points in db
         # Ideas: show accuracy percentage; show progress towards goal
         # #
-        pass
+        print(f"""Let's do some translation exercises. 
+              EXIT: 'exit'
+              RETURN TO MAIN MENU: 'menu'
+            """)
 
+        while True:
+        
+            print(f"Translate the following sentence from {self.target_language} to {self.source_language}")
+
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {API_KEY}"
+            }
+            get_sentence_data = {
+                "model": "gpt-3.5-turbo",
+                "messages": [{
+                    "role": "user", 
+                    "content": f"You are my {self.target_language} language tutor. We are doing a translation exercise. Give me a {self.target_language} sentence to translate into English. The difficulty of this sentence should be C2 (Advanced). Return only the sentence for translation; include no other content."
+                }],
+                "temperature": 0.7
+            }
+            # Make the POST request
+            response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(get_sentence_data))
+
+            # Get the JSON response
+            response_data = response.json()
+
+            # Extract the sentence
+            sentence_for_translation = response_data['choices'][0]['message']['content']
+
+            # Print out the model's response
+            print(sentence_for_translation)
+
+            user_input = input(">>> ")
+
+            if (user_input == "exit"):
+                break
+            elif (user_input == "menu"):
+                self.main_menu()
+                break
+            else:
+                user_translation = user_input
+
+            get_feedback_data = {
+                "model": "gpt-3.5-turbo",
+                "messages": [{
+                    "role": "user", 
+                    "content": f"You are my ${self.target_language} language tutor. We are doing a translation exercise. You gave me the following {self.target_language} to translate into {self.source_language}: {sentence_for_translation}. Here is my translation: {user_translation}. Grade the translation out of 2 points. An incorrect or missing translation should receive 0 points; a partially correct translation should receive 1; and a correct translation should receive 2. Give me concise feedback focusing on errors."
+                }],
+                "temperature": 0.7
+            }
+
+            # Make the POST request
+            response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(get_feedback_data))
+
+            # Get the JSON response
+            response_data = response.json()
+
+            # Extract the sentence
+            tutor_feedback = response_data['choices'][0]['message']['content']
+            print(tutor_feedback)
+        
 
     def vocab_game(self):
         # We could get words from word list and have OpenAI API provide definitions;
@@ -103,6 +202,6 @@ class LanguageBuddy:
 
     def flashcards(self):
         # Users can add words to flashcards from any app stage. Words are stored
-        # in sqlite db and can be reviewed via flashcards. 
+        # in sqlite db and can be reviewed via flashcards.
         # We may pull definitions in via a dictionary API or via OpenAI#
         pass
